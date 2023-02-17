8fd7518ce88046ebec01ecec6c596960a1afab27
Tales B. Albuquerque <101683474+sselat@users.noreply.github.com>
2023-02-17 16:18:36 -0400

    final version

--- coloring-texts-app/src/App.js
+++ coloring-texts-app/src/App.js
@@ -1,13 +1,12 @@
 import TextCard from './components/textCard/textCard.js'
-import ColorPicker from './components/colorPicker/colorPicker.js'
 
 function App() {
   return (
     <div className="App">
-      <TextCard bgColor={'lightgreen'} txtColor={'darkgreen'}/>
-      <TextCard bgColor={'red'} txtColor={'pink'}/>
-      <TextCard bgColor={'pink'} txtColor={'red'}/>
-      <ColorPicker />
+      <TextCard bgColor='lightgreen' txtColor='darkgreen'/>
+      <TextCard bgColor='red' txtColor='pink'/>
+      <TextCard bgColor='blue' txtColor='white'/>
+      <TextCard/>
     </div>
   );
 }
--- coloring-texts-app/src/components/textCard/textCard.js
+++ coloring-texts-app/src/components/textCard/textCard.js
@@ -1,21 +1,17 @@
 import './textCard.css'
 
-import React from 'react'
-
-class TextCard extends React.Component {
-    render () {
-        console.log(this.props)
-        return (
-            <div className='card' style={{backgroundColor:this.props.bgColor, color:this.props.txtColor}}>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, itaque possimus illo harum architecto veniam autem veritatis, neque eum consequatur unde repudiandae nam velit voluptatem ipsam quos impedit ea adipisci?</p>
-            </div>
-        )
-    }
+const TextCard = (props) => {
+    return (
+        <div className='card' style={{backgroundColor:props.bgColor, color:props.txtColor}}>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, itaque possimus illo harum architecto veniam autem veritatis, neque eum consequatur unde repudiandae nam velit voluptatem ipsam quos impedit ea adipisci?</p>
+        </div>
+    )
 }
 
+
 TextCard.defaultProps = {
-    bgColor: 'red',
-    txtColor: 'white'
+    bgColor: 'gold',
+    txtColor: 'blue'
 }
 
 export default TextCard
